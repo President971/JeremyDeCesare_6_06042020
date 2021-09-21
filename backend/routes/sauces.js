1135977b47ec2b053b09299fd482dffbaadb23a4
President971 <decesare971@gmail.com>
2021-09-21 17:21:41 +0200

    final projet

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -10,7 +10,7 @@ const sauceCtrl = require('../controllers/sauces');
 router.get("/", auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post("/", auth, multer, controleSauce, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, controleSauce, sauceCtrl.updateSauce)
+router.put("/:id", auth, multer, controleSauce, sauceCtrl.modifySauce)
 router.delete("/:id", auth, sauceCtrl.deleteSauce)
 router.post("/:id/like", auth, sauceCtrl.likeDislikeSauce)
 
